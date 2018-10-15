c4f7d2871c02b10705c080fff5a13247633721aa
Himavarsha Kagidala <himavarshakagidala@Himavarshas-MacBook-Pro.local>
2018-10-15 00:04:29 -0700

    longest seq

---
+++ src/main/java/com/lab/pc/tree/LogestSeq.java
@@ -0,0 +1,41 @@
+package com.lab.pc.tree;
+
+public class LogestSeq {
+
+	private static int res = 0;
+
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(1);
+		// root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+
+		root.right.left = new TreeNode(2);
+		root.right.right = new TreeNode(4);
+
+		// root.right.left = new TreeNode(6);
+		root.right.right.right = new TreeNode(5);
+		//root.right.right.right.right = new TreeNode(6);
+
+		logestSequenceLen(root, 0, 0);
+
+		System.out.println(res);
+	}
+
+	private static void logestSequenceLen(TreeNode node, int expected, int pathLen) {
+
+		if (node == null)
+			return;
+
+		if (node.val == expected)
+			pathLen++;
+		else
+			pathLen = 1;
+
+		res = Math.max(pathLen, res);
+
+		logestSequenceLen(node.left, node.val + 1, pathLen);
+		logestSequenceLen(node.right, node.val + 1, pathLen);
+
+	}
+
+}
