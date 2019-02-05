bed3d315377c9b1b146dcea250dc85405f5917b8
Bala <balaraju.kagidala@gmail.com>
2019-02-05 15:04:54 -0800

    linked list

---
+++ src/main/java/com/lab/gg/linkedlists/LinkedList.java
@@ -0,0 +1,77 @@
+package com.lab.gg.linkedlists;
+
+public class LinkedList {
+
+	Node head;
+
+	class Node {
+
+		int data;
+		Node next;
+
+		Node(int data) {
+			this.data = data;
+			next = null;
+		}
+	}
+
+	public void push(int newData) {
+
+		Node temp = new Node(newData);
+		temp.next = head;
+		head = temp;
+
+	}
+
+	public void insertAfter(Node previousNode, int data) {
+
+		Node newNode = new Node(data);
+
+		if (previousNode == null) {
+			head = newNode;
+		}
+
+		newNode.next = previousNode.next;
+		previousNode.next = newNode;
+
+	}
+
+	public void append(int data) {
+
+		Node nNode = new Node(data);
+
+		Node temp = head;
+
+		while (temp.next != null)
+			temp = temp.next;
+
+		temp.next = nNode;
+	}
+
+	public void printList() {
+
+		Node temp = head;
+
+		while (temp != null) {
+			System.out.print(temp.data + " ");
+			temp = temp.next;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		LinkedList list = new LinkedList();
+
+		list.push(1);
+		list.push(2);
+		list.push(3);
+
+		list.insertAfter(list.head.next, 33);
+
+		list.append(99);
+		list.append(100);
+
+		list.printList();
+	}
+
+}
