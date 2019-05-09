e5b55b92309f0e3c65b4cc8ec3ae70a115b488a4
Bala <balaraju.kagidala@gmail.com>
2019-05-09 06:25:10 -0700

    comments

--- src/main/java/com/lab/gg/linkedlists/ListInterSection.java
+++ src/main/java/com/lab/gg/linkedlists/ListInterSection.java
@@ -1,7 +1,7 @@
 package com.lab.gg.linkedlists;
 
 /*
- *  NOTE: lists have to be sorted
+ *  NOTE: lists have to be sorted 05092019
  *  1. Start interating through 2 list node.
  *  2. if equal add this to new list
  *  3. first one less move this pointer to next
